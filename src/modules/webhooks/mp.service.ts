0b4ad808e5aaebcfd62c3dcafd6feedc9d98b3d1
Valentin Levenier <valentin.levenier@gmail.com>
2025-12-04 15:55:19 -0300

    tests para sockets print

--- src/modules/webhooks/mp.service.ts
+++ src/modules/webhooks/mp.service.ts
@@ -17,31 +17,31 @@ interface MPPayment {
 
 export const processPaymentWebhook = async (paymentId: string) => {
     
-    // 2. APLICAR EL CASTING (AS MPPayment)
-    const payment = await mpExternalService.getPayment(paymentId) as MPPayment; 
+    // // 2. APLICAR EL CASTING (AS MPPayment)
+    // const payment = await mpExternalService.getPayment(paymentId) as MPPayment; 
 
-    // ¡Ahora TypeScript reconoce "payment.status" sin error!
-    if (payment && payment.status === 'approved') {
+    // // ¡Ahora TypeScript reconoce "payment.status" sin error!
+    // if (payment && payment.status === 'approved') {
         
-        // El orderId lo puedes extraer del external_reference o metadata
-        // Usamos external_reference que enviamos como 'order_ID_XYZ'
-        const orderId = payment.external_reference.replace('order_', ''); 
+    //     // El orderId lo puedes extraer del external_reference o metadata
+    //     // Usamos external_reference que enviamos como 'order_ID_XYZ'
+    //     const orderId = payment.external_reference.replace('order_', ''); 
         
-        if (orderId) {
+    //     if (orderId) {
             
-            // 3. Actualizar la base de datos (DB)
-            await orderService.markOrderPaidService( parseInt(orderId, 10));
+    //         // 3. Actualizar la base de datos (DB)
+    //         await orderService.markOrderPaidService( parseInt(orderId, 10));
             
-            // 4. EMITIR LA NOTIFICACIÓN SOCKET.IO
-            console.log(`[Socket] Pago aprobado. Notificando orden: ${orderId}`);
+    //         // 4. EMITIR LA NOTIFICACIÓN SOCKET.IO
+    //         console.log(`[Socket] Pago aprobado. Notificando orden: ${orderId}`);
             
-            io.emit('payment_update', { 
-                orderId: orderId, 
-                status: 'paid',
-                total: payment.transaction_amount
-            });
-        }
-    } else {
-        console.warn(`[Webhook] Pago ${paymentId} no aprobado. Estado: ${payment?.status}`);
-    }
+    //         io.emit('payment_update', { 
+    //             orderId: orderId, 
+    //             status: 'paid',
+    //             total: payment.transaction_amount
+    //         });
+    //     }
+    // } else {
+    //     console.warn(`[Webhook] Pago ${paymentId} no aprobado. Estado: ${payment?.status}`);
+    // }
 };
