e86aa03e462448f85a75421586e52bc68e3af82b
Valentin Levenier <valentin.levenier@gmail.com>
2025-12-05 23:02:58 -0300

    paymentWebHooks2

--- src/modules/webhooks/mp.routes.ts
+++ src/modules/webhooks/mp.routes.ts
@@ -1,22 +1,39 @@
 import { Router, Request, Response } from 'express';
-import { processPaymentWebhook } from './mp.service';
+import { processOrderWebhook } from './mp.service';
 
 const router = Router();
 
+// router.post('/', async (req: Request, res: Response) => {
+//     try {
+//         console.log("[BEGIN SOCKET WEBHOOK ]");
+//         // Mercado Pago envía el ID del pago en req.body.data.id
+//         const paymentId = req.body?.data?.id;
+//         console.log('[Webhook Received] Payment ID:', paymentId);
+//         console.log(JSON.stringify(req.body));
+//         if (!paymentId) {
+//             return res.status(400).json({ error: 'Payment ID missing' });
+//         }
+//         //const res_ = req.body;
+//         await processPaymentWebhook(paymentId);
+
+//         // Siempre responder 200 a Mercado Pago para confirmar que recibiste el webhook
+//         return res.status(200).send('OK');
+//     } catch (error) {
+//         console.error('[Webhook Error]', error);
+//         return res.status(500).send('Internal Server Error');
+//     }
+// });
+
+// export default router;
+
+
 router.post('/', async (req: Request, res: Response) => {
     try {
-        console.log("[BEGIN SOCKET WEBHOOK ]");
-        // Mercado Pago envía el ID del pago en req.body.data.id
-        const paymentId = req.body?.data?.id;
-        console.log('[Webhook Received] Payment ID:', paymentId);
-        console.log(JSON.stringify(req.body));
-        if (!paymentId) {
-            return res.status(400).json({ error: 'Payment ID missing' });
-        }
-        //const res_ = req.body;
-        await processPaymentWebhook(paymentId);
+        console.log("[WEBHOOK ORDER] Recibido:");
+        console.log(JSON.stringify(req.body, null, 2));
 
-        // Siempre responder 200 a Mercado Pago para confirmar que recibiste el webhook
+        await processOrderWebhook(req.body);
+
         return res.status(200).send('OK');
     } catch (error) {
         console.error('[Webhook Error]', error);
